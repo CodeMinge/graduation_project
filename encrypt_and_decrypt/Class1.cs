ca808b6feffb1e1bf9c31550d0dad248b678884e
CodeMinge <825479572@qq.com>
2017-03-03 18:56:28 -0800

    SQL Server引用dll文档、des加解密

---
+++ encrypt_and_decrypt/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+// 注意：密钥和向量必须为8位，否则加密解密都不成功
+
+namespace ClassLibrary2
+{
+    public partial class DES
+    {
+        public static string Encrypt(string sourceString, string key, string iv)
+        {
+            try
+            {
+                byte[] btKey = Encoding.UTF8.GetBytes(key);
+
+                byte[] btIV = Encoding.UTF8.GetBytes(iv);
+
+                DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    byte[] inData = Encoding.UTF8.GetBytes(sourceString);
+                    try
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(btKey, btIV), CryptoStreamMode.Write))
+                        {
+                            cs.Write(inData, 0, inData.Length);
+
+                            cs.FlushFinalBlock();
+                        }
+
+                        return Convert.ToBase64String(ms.ToArray());
+                    }
+                    catch
+                    {
+                        return sourceString;
+                    }
+                }
+            }
+            catch { }
+
+            return "DES加密出错";
+        }
+
+        //调用：  string recordString = Encrypt("金胖子死了", "20111219", "12345678");
+        // 结果：recordString= "xQ969nexy964SXhkTuekUQ=="
+
+        public static string Decrypt(string encryptedString, string key, string iv)
+        {
+            byte[] btKey = Encoding.UTF8.GetBytes(key);
+
+            byte[] btIV = Encoding.UTF8.GetBytes(iv);
+
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                byte[] inData = Convert.FromBase64String(encryptedString);
+                try
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(btKey, btIV), CryptoStreamMode.Write))
+                    {
+                        cs.Write(inData, 0, inData.Length);
+
+                        cs.FlushFinalBlock();
+                    }
+
+                    return Encoding.UTF8.GetString(ms.ToArray());
+                }
+                catch
+                {
+                    return encryptedString;
+                }
+            }
+        }
+    }
+}
+ 
