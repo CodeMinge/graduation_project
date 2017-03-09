59d88f9fcf2e6d714b0ec72b8d96f2beb881b5ed
CodeMinge <825479572@qq.com>
2017-03-08 20:27:14 -0800

    锁与事务的加入

--- application_program/client/Client.java
+++ application_program/client/Client.java
@@ -11,11 +11,13 @@ import server.Server;
  */
 public class Client {
 
-	public static void main(String[] args) {
-		Socket socket = null;
-		BufferedReader br = null;
-		PrintWriter pw = null;
-		Scanner scanner = new Scanner(System.in);// 从键盘读取
+	public Socket socket = null;
+	public BufferedReader br = null;
+	public PrintWriter pw = null;
+	public Scanner scanner = new Scanner(System.in);// 从键盘读取
+	public ReadServerMessage rsm = null;
+
+	public Client() {
 		try {
 			// 创建客户端socket
 			socket = new Socket(Server.HOST, Server.PORT);
@@ -23,82 +25,128 @@ public class Client {
 			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			// 写入信息到服务器端
 			pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
-			new ReadServerMessage(socket, br);// 从服务器读取消息
-			
-			selfTest(pw);
-//			while (true) {
-//				String temp = scanner.nextLine();// 从键盘读取一行
-//				pw.println(temp);// 写到服务器
-//				pw.flush();
-//				if (temp.equals("q")) {
-//					break;
-//				}
-//			}
+
+			rsm = new ReadServerMessage(socket, br);// 从服务器读取消息
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+//		selfTest();
+//		interactTest();
+		encrypt_decrypt_mix_test();
+	}
+
+	public void close() {
+		try {
+			System.out.println("close......");
+			br.close();
+			br = null;
+			pw.close();
+			pw = null;
+			socket.close();
+			socket = null;
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				System.out.println("close......");
-				br.close();         br = null;
-				pw.close();         pw = null;
-				socket.close();     socket = null;
-			} catch (Exception e) {
-				e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 交互测试
+	 */
+	public static void interactTest() {
+		Client client = new Client();
+		
+		while (true) {
+			String temp = client.scanner.nextLine();// 从键盘读取一行
+			client.pw.println(temp);// 写到服务器
+			client.pw.flush();
+			if (temp.equals("q")) {
+				break;
 			}
 		}
 	}
-	
+
 	/**
-	 * 自测程序，两个测试之间要有停顿，否则出错，我这里的停顿较长
+	 * 自测程序
 	 */
-	public static void selfTest(PrintWriter pw) {
-//		loginSelfTest(pw);
-//		
+	public static void selfTest() {
+		Client client = new Client();
+
+//		loginSelfTest(client);
+//
 //		try {
-//			Thread.sleep(10000);
+//			Thread.sleep(10000); // 两个测试之间要有停顿，否则出错，我这里的停顿较长
 //		} catch (InterruptedException e) {
 //			e.printStackTrace();
 //		}
-//		
-//		quitSelfTest(pw);
-		
-//		encryptSelfTest(pw);
-		decryptSelfTest(pw);
+//
+//		quitSelfTest(client);
+//
+//		encryptSelfTest(client);
+		decryptSelfTest(client);
+
+		client.close();
 	}
 
 	/**
 	 * 退出命令自测程序
 	 */
-	public static void quitSelfTest(PrintWriter pw) {
+	public static void quitSelfTest(Client client) {
 		String temp = "q";
-		pw.println(temp);// 写到服务器
-		pw.flush();
+		client.pw.println(temp);// 写到服务器
+		client.pw.flush();
 	}
-	
+
 	/**
 	 * 登录命令自测程序
 	 */
-	public static void loginSelfTest(PrintWriter pw) {
+	public static void loginSelfTest(Client client) {
 		String temp = "login jd 金胖子死了";
-		pw.println(temp);// 写到服务器
-		pw.flush();
+		client.pw.println(temp);// 写到服务器
+		client.pw.flush();
 	}
-	
+
 	/**
 	 * 加密独立测试程序
 	 */
-	public static void encryptSelfTest(PrintWriter pw) {
+	public static void encryptSelfTest(Client client) {
 		String temp = "encrypt worker_table card_number";
-		pw.println(temp);// 写到服务器
-		pw.flush();
+		client.pw.println(temp);// 写到服务器
+		client.pw.flush();
 	}
-	
+
 	/**
 	 * 解密独立测试程序
 	 */
-	public static void decryptSelfTest(PrintWriter pw) {
+	public static void decryptSelfTest(Client client) {
 		String temp = "decrypt worker_table card_number";
-		pw.println(temp);// 写到服务器
-		pw.flush();
+		client.pw.println(temp);// 写到服务器
+		client.pw.flush();
+	}
+
+	/**
+	 * 加密解密混合测试程序 这个测试主要是测试锁和事务的
+	 */
+	public static void encrypt_decrypt_mix_test() {
+		Client c1 = new Client();
+		Client c2 = new Client();
+		String temp1 = "encrypt worker_table card_number";
+		c1.pw.println(temp1);// 写到服务器
+		c1.pw.flush();
+		
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String temp2 = "decrypt worker_table card_number";
+		c2.pw.println(temp2);// 写到服务器
+		c2.pw.flush();
 	}
 }
--- application_program/server/Command.java
+++ application_program/server/Command.java
@@ -1,27 +1,26 @@
 package server;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 import message_center.ServerMessage;
+
 /**
- * 先支持几个简单的命令行
- *  1、q --------退出
- *  2、login 用户名 密码 --------登录
- *  3、encrypt  table（表名） property（属性名） --------加密，实质是设置敏感属性
- *  4、decrypt  table（表名） property（属性名） -------解密
+ * 先支持几个简单的命令行 1、q --------退出 2、login 用户名 密码 --------登录 3、encrypt table（表名）
+ * property（属性名） --------加密，实质是设置敏感属性 4、decrypt table（表名） property（属性名）
+ * -------解密
  */
 
 public class Command {
-	
+
 	public static final String COMMAND_QIUT = "q";
 	public static final String COMMAND_LOGIN = "login";
 	public static final String COMMAND_ENCRYPT = "encrypt";
 	public static final String COMMAND_DECRYPT = "decrypt";
-	
+
+	private static Object mylock = new Object(); // static确保只有一把锁
+
 	private String command = null;
-	private KeyAndVector kav = new KeyAndVector(); //密钥向量生成器
+	private KeyAndVector kav = new KeyAndVector(); // 密钥向量生成器
 
 	public void setCommand(String target) {
 		command = target;
@@ -29,7 +28,9 @@ public class Command {
 
 	/**
 	 * 对用户发送过来的命令做解析处理
-	 * @param target  命令
+	 * 
+	 * @param target
+	 *            命令
 	 * @param dbc
 	 * @return 信息id
 	 */
@@ -47,16 +48,19 @@ public class Command {
 		} else if (commandArr[0].equals(COMMAND_DECRYPT)) {
 			return decryptProcess(commandArr[1], commandArr[2], dbc);
 		}
-		
+
 		return ServerMessage.NULL;
 	}
 
 	/**
 	 * 登录命令解析
-	 * @param userName 用户名
-	 * @param password 密码
+	 * 
+	 * @param userName
+	 *            用户名
+	 * @param password
+	 *            密码
 	 * @param dbc
-	 * @return   信息id
+	 * @return 信息id
 	 */
 	private String loginProcess(String userName, String password, DatabaseConnection dbc) {
 		String res = ServerMessage.NULL;
@@ -76,20 +80,20 @@ public class Command {
 				String temp = rs.getString(1);
 				System.out.println(password + " t");
 				System.out.println(temp + " t");
-				
-				sql = "SELECT [graduation_project].[dbo].[DESDecrypt]('"+ temp +"', '20111219', '12345678');";
+
+				sql = "SELECT [graduation_project].[dbo].[DESDecrypt]('" + temp + "', '20111219', '12345678');";
 				pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);
 				ResultSet rs2 = pstmt.executeQuery();
-				
-				while(rs2.next()) {
+
+				while (rs2.next()) {
 					temp = rs2.getString(1);
 					break;
 				}
-				
+
 				System.out.println(password + " t");
 				System.out.println(temp + " t");
-				
-				if(temp.equals(password)) {
+
+				if (temp.equals(password)) {
 					res = ServerMessage.LOGINSUCCESS;
 					break;
 				}
@@ -97,181 +101,235 @@ public class Command {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return res;
 	}
-	
+
 	/**
 	 * 加密信息解析，对某表的某列进行加密，某表的某列的前提是未经过加密
-	 * @param table        表名
-	 * @param property       列名
+	 * 
+	 * @param table
+	 *            表名
+	 * @param property
+	 *            列名
 	 * @param dbc
-	 * @return   信息id
+	 * @return 信息id
 	 */
 	private String encryptProcess(String table, String property, DatabaseConnection dbc) {
 		String res = ServerMessage.NULL;
 		// 确保表和列存在
 		res = tbExists(table, dbc);
-		if(res.equals(ServerMessage.NOTABLE))
+		if (res.equals(ServerMessage.NOTABLE))
 			return res;
-		
-		ServerMessage.ServerMessageOutput(ServerMessage.EXISTTABLE); //定位信息
-		
-		res = propertyExists(table, property, dbc);
-		if(res.equals(ServerMessage.NOPROPERTY))
-			return res;
-		
-		ServerMessage.ServerMessageOutput(ServerMessage.EXISTPROPERTY); //定位信息
-		
-		res = ServerMessage.ENCRYPTSUCCESS;
-	    // 首先取得密钥和向量
-		String key = kav.getKey();
-		String vt = kav.getVector();
-		System.out.println(key + " " + vt);
-		
-		String sql = "SELECT * from [graduation_project].[dbo].[" + table + "]";
 
-		PreparedStatement pstmt = null;
-		try {
-			pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);
-			ResultSet rs = pstmt.executeQuery();
-			while (rs.next()) { //不停地进行加密
-				//取得加密数据
-				String temp = rs.getString(1);    //这里，我的表必须是以第一列为主键，而且不能对第一列进行加密
-				String target = rs.getString(property); //这是将要加密的数据
-				System.out.println(temp + " " + target);
-				
-				//执行加密操作,temp1是加密后的结果
-				String temp1 = "";
-				sql = "SELECT [graduation_project].[dbo].[Des_Encrypt]('"+ target +"', '" + key + "', '" + vt + "')";
+		ServerMessage.ServerMessageOutput(ServerMessage.EXISTTABLE); // 定位信息
+
+		res = propertyExists(table, property, dbc);
+		if (res.equals(ServerMessage.NOPROPERTY))
+			return res;
+
+		ServerMessage.ServerMessageOutput(ServerMessage.EXISTPROPERTY); // 定位信息
+
+		// 加锁，保证加密是流程的完整性
+		synchronized (mylock) {
+			res = ServerMessage.ENCRYPTSUCCESS;
+			// 首先取得密钥和向量
+			String key = kav.getKey();
+			String vt = kav.getVector();
+			System.out.println(key + " " + vt);
+
+			String sql = "SELECT * from [graduation_project].[dbo].[" + table + "]";
+
+			PreparedStatement pstmt = null;
+			try {
+
+				boolean autoCommit = dbc.dbConn.getAutoCommit();
+				// 关闭自动提交功能
+				dbc.dbConn.setAutoCommit(false);
+
 				pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);
-				ResultSet rs2 = pstmt.executeQuery();
-				while(rs2.next()) {
-					temp1 = rs2.getString(1);
-					break;
+				ResultSet rs = pstmt.executeQuery();
+				while (rs.next()) { // 不停地进行加密
+					// 取得加密数据
+					String temp = rs.getString(1); // 这里，我的表必须是以第一列为主键，而且不能对第一列进行加密
+					String target = rs.getString(property); // 这是将要加密的数据
+					System.out.println(temp + " " + target);
+
+					// 执行加密操作,temp1是加密后的结果
+					String temp1 = "";
+					sql = "SELECT [graduation_project].[dbo].[Des_Encrypt]('" + target + "', '" + key + "', '" + vt
+							+ "')";
+					pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);
+					ResultSet rs2 = pstmt.executeQuery();
+					while (rs2.next()) {
+						temp1 = rs2.getString(1);
+						break;
+					}
+					System.out.println(target + " " + temp1);
+
+					// 更新表，将加密后的内容更新到表中
+					sql = "UPDATE [graduation_project].[dbo].[" + table + "] SET " + property + " = '" + temp1
+							+ "' WHERE " + property + " = '" + target + "'";
+					pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);
+					int i = pstmt.executeUpdate();
+					if (i == 0) { // 检测加密情况
+						res = ServerMessage.ENCRYPTFAIL;
+					}
 				}
-				System.out.println(target + " " + temp1);
-				
-				//更新表，将加密后的内容更新到表中
-				sql = "UPDATE [graduation_project].[dbo].["+ table + "] SET " + property + " = '"+  temp1 + "' WHERE " + property + " = '" + target + "'";
+				// 记录加密信息
+				sql = "insert into message_tb(tb_name,property,algorithm,secret_key,vector) VALUES(?,?,?,?,?)";
 				pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);
-				int i =  pstmt.executeUpdate();
-				if(i == 0) {         //检测加密情况
-					res = ServerMessage.ENCRYPTFAIL;
+				pstmt.setString(1, table);
+				pstmt.setString(2, property);
+				pstmt.setString(3, "des"); // 现在只有des算法
+				pstmt.setString(4, key);
+				pstmt.setString(5, vt);
+				pstmt.executeUpdate();
+
+				// 提交事务
+				dbc.dbConn.commit();
+				// 恢复原来的提交模式
+				dbc.dbConn.setAutoCommit(autoCommit);
+			} catch (SQLException e) { // 只要其中有一个sql执行错误，就应该回滚
+				res = ServerMessage.ENCRYPTFAIL;
+				try {
+					// 回滚、取消前述操作
+					dbc.dbConn.rollback();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
 				}
+				e.printStackTrace();
 			}
-			//记录加密信息
-			sql= "insert into message_tb(tb_name,property,algorithm,secret_key,vector) VALUES(?,?,?,?,?)";
-			pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);
-			pstmt.setString(1, table);
-			pstmt.setString(2, property); 
-			pstmt.setString(3, "des"); //现在只有des算法
-			pstmt.setString(4, key); 
-			pstmt.setString(5, vt); 
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
 		}
-		
+
 		return res;
 	}
-	
+
 	/**
 	 * 解密信息解析，对某表的某列进行解密，某表的某列的前提是已经过加密
-	 * @param table        表名
-	 * @param property       列名
+	 * 
+	 * @param table
+	 *            表名
+	 * @param property
+	 *            列名
 	 * @param dbc
-	 * @return   信息id
+	 * @return 信息id
 	 */
 	private String decryptProcess(String table, String property, DatabaseConnection dbc) {
 		String res = ServerMessage.NULL;
 		// 确保表和列存在
 		res = tbExists(table, dbc);
-		if(res.equals(ServerMessage.NOTABLE))
+		if (res.equals(ServerMessage.NOTABLE))
 			return res;
-		
-		ServerMessage.ServerMessageOutput(ServerMessage.EXISTTABLE); //定位信息
-		
+
+		ServerMessage.ServerMessageOutput(ServerMessage.EXISTTABLE); // 定位信息
+
 		res = propertyExists(table, property, dbc);
-		if(res.equals(ServerMessage.NOPROPERTY))
+		if (res.equals(ServerMessage.NOPROPERTY))
 			return res;
-		
-		ServerMessage.ServerMessageOutput(ServerMessage.EXISTPROPERTY); //定位信息
-		
-		res = ServerMessage.DECRYPTSUCCESS;
-		String key = "";
-		String vt = "";
 
-		String sql = "SELECT * from [graduation_project].[dbo].[message_tb] where tb_name = '" + table + "' and property = '" + property + "'";
+		ServerMessage.ServerMessageOutput(ServerMessage.EXISTPROPERTY); // 定位信息
 
-		PreparedStatement pstmt = null;
-		try {
-			pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);
-			ResultSet rs = pstmt.executeQuery();
-			while(rs.next()) {    // 取得解密的密钥和向量
-				key = rs.getString("secret_key");
-				vt = rs.getString("vector");
-				break;  // 这里只能有一行
-			}
-			System.out.println(key + " " + vt);
-			
-			sql = "SELECT * from [graduation_project].[dbo].[" + table + "]";
-			pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);
-			rs = pstmt.executeQuery();
-			while(rs.next()) {       //不停地进行解密
-				//取得加密数据
-				String temp = rs.getString(1);    //这里，我的表必须是以第一列为主键，而且不能对第一列进行加密
-				String target = rs.getString(property); //这是将要解密的数据
-				System.out.println(temp + " " + target);
+		synchronized (mylock) {
+			res = ServerMessage.DECRYPTSUCCESS;
+			String key = "";
+			String vt = "";
+
+			String sql = "SELECT * from [graduation_project].[dbo].[message_tb] where tb_name = '" + table
+					+ "' and property = '" + property + "'";
+
+			PreparedStatement pstmt = null;
+			try {
 				
-				//执行解密操作,temp1是解密后的结果
-				String temp1 = "";
-				sql = "SELECT [graduation_project].[dbo].[Des_Decrypt]('"+ target +"', '" + key + "', '" + vt + "')";
-				pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);
-				ResultSet rs2 = pstmt.executeQuery();
-				while(rs2.next()) {
-					temp1 = rs2.getString(1);
-					break;
-				}
-				System.out.println(target + " " + temp1);
+				boolean autoCommit = dbc.dbConn.getAutoCommit();
+				// 关闭自动提交功能
+				dbc.dbConn.setAutoCommit(false);
 				
-				//更新表，将解密后的内容更新到表中
-				sql = "UPDATE [graduation_project].[dbo].["+ table + "] SET " + property + " = '"+  temp1 + "' WHERE " + property + " = '" + target + "'";
 				pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);
-				int i =  pstmt.executeUpdate();
-				if(i == 0) {         //检测解密情况
-					res = ServerMessage.DECRYPTFAIL;
+				ResultSet rs = pstmt.executeQuery();
+				while (rs.next()) { // 取得解密的密钥和向量
+					key = rs.getString("secret_key");
+					vt = rs.getString("vector");
+					break; // 这里只能有一行
 				}
+				System.out.println(key + " " + vt);
+//				if(key.equals("") || vt.equals(""))
+//					throw SQLException;
+				// 当这个key和vt不存在的时候，解密是一定会失败的，这个时候应该会回滚
+
+				sql = "SELECT * from [graduation_project].[dbo].[" + table + "]";
+				pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);
+				rs = pstmt.executeQuery();
+				while (rs.next()) { // 不停地进行解密
+					// 取得加密数据
+					String temp = rs.getString(1); // 这里，我的表必须是以第一列为主键，而且不能对第一列进行加密
+					String target = rs.getString(property); // 这是将要解密的数据
+					System.out.println(temp + " " + target);
+
+					// 执行解密操作,temp1是解密后的结果
+					String temp1 = "";
+					sql = "SELECT [graduation_project].[dbo].[Des_Decrypt]('" + target + "', '" + key + "', '" + vt
+							+ "')";
+					pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);
+					ResultSet rs2 = pstmt.executeQuery();
+					while (rs2.next()) {
+						temp1 = rs2.getString(1);
+						break;
+					}
+					System.out.println(target + " " + temp1);
+
+					// 更新表，将解密后的内容更新到表中
+					sql = "UPDATE [graduation_project].[dbo].[" + table + "] SET " + property + " = '" + temp1
+							+ "' WHERE " + property + " = '" + target + "'";
+					pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);
+					int i = pstmt.executeUpdate();
+					if (i == 0) { // 检测解密情况
+						res = ServerMessage.DECRYPTFAIL;
+					}
+				}
+				// 记录解密信息，实质是删除原来的的加密信息
+				sql = "DELETE FROM message_tb WHERE tb_name = '" + table + "' and property = '" + property + "'";
+				pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);
+				pstmt.executeUpdate();
+				
+				// 提交事务
+				dbc.dbConn.commit();
+				// 恢复原来的提交模式
+				dbc.dbConn.setAutoCommit(autoCommit);
+			} catch (SQLException e) {
+				res = ServerMessage.DECRYPTFAIL;
+				try {
+					// 回滚、取消前述操作
+					dbc.dbConn.rollback();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+				e.printStackTrace();
 			}
-			//记录解密信息，实质是删除原来的的加密信息
-			sql= "DELETE FROM message_tb WHERE tb_name = '" + table +"' and property = '" + property + "'";
-			pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
 		}
-		
 		return res;
 	}
-	
+
 	/**
 	 * 确认数据库中是否存在表
-	 * @param table 表名
+	 * 
+	 * @param table
+	 *            表名
 	 * @param dbc
 	 * @return
 	 */
 	private String tbExists(String table, DatabaseConnection dbc) {
 		String res = ServerMessage.NOTABLE;
-		
-		String sql = "select * from sys.tables"; //查询所有的表
+
+		String sql = "select * from sys.tables"; // 查询所有的表
 
 		PreparedStatement pstmt = null;
 		try {
-			pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);			
+			pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);
 			ResultSet rs = pstmt.executeQuery();
 			while (rs.next()) {
 				String temp = rs.getString(1); // 取出的列与表的名称对应
-				if(table.equals(temp)) {
+				if (table.equals(temp)) {
 					System.out.println(table + " " + temp);
 					res = ServerMessage.EXISTTABLE;
 					break;
@@ -280,30 +338,33 @@ public class Command {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return res;
 	}
-	
+
 	/**
 	 * 确认表中是否存在某列
-	 * @param table      表名
-	 * @param property   列名
+	 * 
+	 * @param table
+	 *            表名
+	 * @param property
+	 *            列名
 	 * @param dbc
 	 * @return
 	 */
 	private String propertyExists(String table, String property, DatabaseConnection dbc) {
 		String res = ServerMessage.NOPROPERTY;
-		
-		//这个sql命令的结果只有一列name，而列名从上到下排列
-		String sql = "select name from syscolumns where id = object_id('graduation_project.dbo." + table + "')"; //查询所有的表
+
+		// 这个sql命令的结果只有一列name，而列名从上到下排列
+		String sql = "select name from syscolumns where id = object_id('graduation_project.dbo." + table + "')"; // 查询所有的列
 
 		PreparedStatement pstmt = null;
 		try {
-			pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);			
+			pstmt = (PreparedStatement) dbc.dbConn.prepareStatement(sql);
 			ResultSet rs = pstmt.executeQuery();
 			while (rs.next()) {
-				String temp = rs.getString(1); 
-				if(property.equals(temp)) {
+				String temp = rs.getString(1);
+				if (property.equals(temp)) {
 					System.out.println(property + " " + temp);
 					res = ServerMessage.EXISTPROPERTY;
 					break;
@@ -312,7 +373,7 @@ public class Command {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return res;
 	}
 }
